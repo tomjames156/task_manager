cfb0ebfb57bde79637de8f31e4f9f92b6c758bcd
Akinwande Tomisin <tomjames156@gmail.com>
2023-09-05 10:50:27 +0100

    added the search functionality

---
+++ src/pages/templates/PublicUserProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PublicUserProfile() {
+  return (
+    <div>PublicUserProfile</div>
+  )
+}
+
+export default PublicUserProfile
--- src/pages/templates/SearchUsers.jsx
+++ src/pages/templates/SearchUsers.jsx
@@ -1,39 +1,61 @@
 import Header from "../../components/sectioning/Header"
 import Loader from "../../components/items/Loader"
-import { useEffect, useState } from "react"
+import { useState, useRef } from "react"
 import UserItem from "../../components/items/UserItem"
 
 function SearchUsers() {
-    const [searchInput, setSearchInput] = useState('')
+    const searchQueryRef = useRef()
     const [users, setUsers] = useState([])
+    const [searchMessage, setSearchMessage] = useState('Find your friends on Taskify 🔍')
     const [isLoading, setIsLoading] = useState(false)
     const api = process.env.REACT_APP_API_LINK
+    const submitRef = useRef()
 
-    const getAllUsers = async () => {
+    const searchUsers = async (e) => {
         setIsLoading(true)
-        let response = await fetch(`${api}/users`)
-        let data = await response.json()
-        // console.log(data)
-        setUsers(data)
+        e.preventDefault()
+        try{
+            if(searchQueryRef.current.value !== ''){
+                let response = await fetch(`${api}/users/`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        "query": searchQueryRef.current.value
+                    })
+                })
+                let data = await response.json()
+                setUsers(data)
+                setSearchMessage('No friends found 🥲')
+            }
+        }catch(err){
+            console.log(err)
+        }
+
+        if(searchQueryRef.current.value === ''){
+            setUsers([])
+            setSearchMessage("Find your friends on Taskify 🔍")
+        }
         setIsLoading(false)
     }
 
-    useEffect(() => {
-        getAllUsers()
-    }, [])
+    const handleChange = (e) => {
+        submitRef.current.click()
+    }
 
   return (
     <div className="container">
         <Header/>
         <main>
-            {isLoading ? <Loader/> : 
             <div>
-                <form action="" className="search-form">
-                    <input type="text" onChange={(e) => setSearchInput(e.target.value)} placeholder="Search" value={searchInput || '' }/>
-                    <button><i className="fa-solid fa-magnifying-glass"></i></button>
+                <form autoComplete="off" onSubmit={searchUsers} className="search-form">
+                    <input autoFocus type="text" name="query" onChange={handleChange} placeholder="Search" ref={searchQueryRef}/>
+                    <button ref={submitRef} type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
                 </form>
-                {users.length > 0 ? users.map((user) => <UserItem key={user.id} user_obj={user} />) : 'Content' }
-            </div>}
+                {isLoading  ? <Loader/> : users.length > 0 ? users.map((user, index) => <UserItem key={index} user_obj={user} />) : searchMessage }
+                
+            </div>
         </main>
     </div>
   )
